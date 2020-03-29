a8323e89b8db310600343e288b9683b2907ee705
jdj2261 <wlseowhd12@naver.com>
2020-03-29 17:52:30 +0900

    delete files

--- using-python/nav_manager/build/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/djjin/Work/test_ws/src/nav_manager/build/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
